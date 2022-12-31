2e97ed14f209c1a2ad12b6b12a0b4bcb4f4318c5
Asmaul-Shahana <jucse28.360@gmail.com>
2022-12-31 23:07:34 +0600

    Different types of views

---
+++ Day5project360/Day5app360/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Employee, Department
+
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Department)
+
+### Superuser: adminn
+### password: 12345
